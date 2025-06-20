e550f9947db424875081d493e6ee47ac0e98d5ea
MKNAPJ <lisna.junitadaeli@cell-a.id>
2025-06-20 14:12:47 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
-=======
-export default function Home() {
-<<<<<<<< HEAD:app/page.tsx
-  return (
-    <main>
-      <h1>Selamat datang di Luminous Skincare Stock Opname</h1>
-    </main>
-  )
-}
-========
-  return <main className="p-4">Stock Opname Homepage</main>;
-}
->>>>>>>> c48946dd6d75a83a973689d9c8596f2876cd775b:app/layout.tsx
->>>>>>> c48946dd6d75a83a973689d9c8596f2876cd775b
